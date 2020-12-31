260ff1d03d6c7e12914cd8b4e683a2a76c9152cd
alaahosny9 <62642297+alaahosny9@users.noreply.github.com>
2020-12-31 22:50:20 +0200

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,14 @@
+public class Customer extends Person {
+    Customer(){
+    }
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+}
